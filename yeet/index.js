ce13f619406d0701a6ff822627c9cc0a14118bc1
Mason DiGiorgio <31572670+MasonDiGi@users.noreply.github.com>
2020-12-03 07:21:44 -0500

    added a file

--- yeet/index.js
+++ yeet/index.js
@@ -1,1 +1,44 @@
-hey this is a txt file lmao
+const Discord = require('discord.js');
+const TOKEN = "NTM2NzU3MjE0OTIxMjI4Mjkx.DybVpw.pBzU9P-cGEu0eBx3PBp8h8O1nHk";
+const prefix = "!";
+const fs = require("fs");
+
+const bot = new Discord.Client();
+bot.commands = new Discord.Collection();
+
+fs.readdir("./commands", (err, files) => {
+    if (err) console.log(err);
+
+    let jsfile = files.filter(f => f.split(".").pop() == "js");
+    if (jsfile.length <= 0) {
+        console.log("Couldn't find 'commands'");
+        return;
+    }
+
+    jsfile.forEach((f, i) => {
+        let props = require(`./commands/${f}`);
+        console.log(`${f} loaded`);
+        bot.commands.set(props.help.name, props);
+    });
+});
+
+
+bot.on('ready', async () => {
+    console.log(`${bot.user.username} is online.`);
+    bot.user.setActivity("everything", {type: "LISTENING"})
+});
+
+
+bot.on("message", async message => {
+    if (message.author.bot) return;
+    if (message.channel.type == "dm") return;
+
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+    let commandfile = bot.commands.get(cmd.slice(prefix.length));
+    if (commandfile) commandfile.run(bot, message, args);
+});
+
+
+bot.login(TOKEN);
